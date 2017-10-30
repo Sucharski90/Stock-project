fc342d2d9cfab4af4793119b48b8c592eebc16f2
Matthew Sucharski <a7x1013@aol.com>
2017-10-30 14:34:40 -0400

    2:34pm 10/30 views edited

---
+++ models/stock.js
@@ -0,0 +1,58 @@
+const db = require('../db/config')
+
+const Stock = {}
+
+Stock.findAll = () =>
+  db.query('SELECT * FROM favstock')
+
+Stock.findById = id =>
+  db.one('SELECT * FROM favstock WHERE id = $1',[id])
+
+Stock.create = user => db.one(`
+  INSERT INTO favstock (
+    symbol,
+    companyName,
+    primaryExchange,
+    sector,
+    open,
+    openTime,
+    close,
+    closeTime,
+    latestPrice,
+
+  ) VALUES (
+    $/symbol/,
+    $/companyName/,
+    $/primaryExchange/,
+    $/sector/.
+    $/open/,
+    $/openTime/,
+    $/close/,
+    $/closeTime/,
+    $/latestPrice/
+  )
+  RETURNING *`,
+  stockusers
+)
+
+User.delete = id =>
+  db.none(`DELETE FROM stockusers WHERE id=$1`,id)
+
+User.update = stockusers =>
+  db.one(`
+    UPDATE users SET
+      symbol=$1,
+      companyName=$2,
+      primaryExchange=$3,
+      sector=$4,
+      open=$5,
+      openTime=$6,
+      close=$7,
+      closeTime=$8,
+      latestPrice=$9
+    WHERE
+      id=$1
+    RETURNING *`,
+    users)
+
+module.exports = User
