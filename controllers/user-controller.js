93758bc0ee2711cfe7ea30959400de953f3f71e1
Matthew Sucharski <a7x1013@aol.com>
2017-10-30 20:47:21 -0400

    app still crashes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const bcrypt = require('bcryptjs');
 
 const userController = {}
 
@@ -60,5 +61,36 @@ userController.delete = (req,res) => {
       res.status(500).json(err)
     })
 }
+
+userController.create = (req, res) => {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password_digest: hash,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  }).then(user => {
+    req.login(user, (err) => {
+      if (err) return next(err);
+      res.redirect('/user');
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+}
+
+module.exports = userController;
+
+userController.index = (req, res) => {
+  res.json({
+    message: 'Put a user profile page on this route',
+    data: {
+      user: req.user,
+    },
+  });
+};
 
 module.exports = userController
